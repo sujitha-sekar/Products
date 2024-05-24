69837f92407c6495bc40866fdc14ba2bb4a06ebb
sujitha-sekar <snowashii2002@gmail.com>
2024-05-24 12:22:38 +0530

    primary color updation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './core/services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TestAngularProject';
+
+  constructor(private productService: ProductsService){}
+
+  ngOnInit(): void {
+    this.productService.getMessage();
+  }
 }
--- src/app/core/components/product-details/product-details.component.ts
+++ src/app/core/components/product-details/product-details.component.ts
@@ -13,32 +13,35 @@ import { HttpRoutingService } from '../../services/http-routing.service';
 export class ProductDetailsComponent implements OnInit {
 
   productsForm!: FormGroup;
-  category: any; 
+  category: any;
   updateData: boolean = false;
   paramData = {
     id: '',
     data: ''
   }
   productDetails: any;
+  messages: any;
 
   constructor(private snackbarService: SnackbarService,
     private productService: ProductsService,
     private route: ActivatedRoute,
     private httpService: HttpRoutingService,
     private router: Router
-  ){}
+  ) { }
 
   ngOnInit(): void {
 
+    this.productService.message.subscribe(res => this.messages = res);
+
     this.route.params.subscribe((res: any) => {
       console.log(res);
-      if(res && res?.id) {
+      if (res && res?.id) {
         this.paramData.id = res?.id;
         this.paramData.data = res?.data;
         this.httpService.getProductById(this.paramData.id).subscribe((res: any) => {
-          this.productDetails = res.productDetails;        
+          this.productDetails = res.productDetails;
           console.log('productDetails', this.productDetails);
-          if(this.productDetails) {
+          if (this.productDetails) {
             this.updateData = true;
             this.formInitalize();
           }
@@ -48,53 +51,59 @@ export class ProductDetailsComponent implements OnInit {
 
     this.productsForm = new FormGroup({
       name: new FormControl(null, Validators.required),
-      description: new FormControl(null, Validators.required),
-      price: new FormControl(null, Validators.required)
+      description: new FormControl(null),
+      price: new FormControl(null, [Validators.required, Validators.min(0.01), Validators.pattern(/^\d+(\.\d{1,2})?$/)])
     });
   };
 
   formInitalize() {
     this.productsForm = new FormGroup({
       name: new FormControl(this.productDetails && this.productDetails?.name ? this.productDetails?.name : null, Validators.required),
-      description: new FormControl( this.productDetails && this.productDetails ? this.productDetails?.description : null, Validators.required),
-      price: new FormControl(this.productDetails && this.productDetails ? this.productDetails?.price : null, Validators.required)
+      description: new FormControl(this.productDetails && this.productDetails ? this.productDetails?.description : null),
+      price: new FormControl(this.productDetails && this.productDetails ? this.productDetails?.price : null, [Validators.required, Validators.min(0.01), Validators.pattern(/^\d+(\.\d{1,2})?$/)])
     });
   }
 
   onSave() {
-    if(this.productsForm.valid) {
+    if (this.productsForm.valid) {
       console.log('Form: ', this.productsForm.value);
-      if(!this.updateData) {
+      if (!this.updateData) {
         this.productService.createProduct(this.productsForm.value).subscribe((res: any) => {
-          if(res) {
+          if (res) {
             this.productsForm.reset();
             console.log('Created:', res);
             this.snackbarService.openSnackbar('Product Saved successfully', 'Success');
-          } else {
+          }
+        }, (err) => {
+          if (err) {
             this.snackbarService.openSnackbar('Error occured while create the product', 'Error')
           }
         });
       } else {
-        console.log('Check:', this.productsForm)
         this.productsForm.value.id = +this.paramData?.id;
-        if(this.productsForm.dirty) {
-          this.httpService.updateProduct(this.productsForm.value.id,this.productsForm.value).subscribe((res: any) => {
-            if(res) {
+        if (this.productsForm.dirty) {
+          this.httpService.updateProduct(this.productsForm.value.id, this.productsForm.value).subscribe((res: any) => {
+            if (res) {
               this.productsForm.markAsPristine();
               this.snackbarService.openSnackbar('Product updated Successfully', 'Success');
               this.router.navigate(['/app/productList']);
-            } else {
+            }
+          }, (err) => {
+            if (err) {
               this.snackbarService.openSnackbar('Error occured while update product', 'Error');
             }
           });
         } else {
           this.snackbarService.openSnackbar('You have not change any product values', 'Warning', 5000)
-        } 
-
+        }
       }
     } else {
       this.snackbarService.openSnackbar('Please fill the mantatory field', 'Warning');
     }
   };
+
+  onNavigate() {
+    this.router.navigate(['/app/productList']);
+  }
 
 }
